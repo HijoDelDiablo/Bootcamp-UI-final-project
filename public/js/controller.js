8796251988a0a810f4af03c92bcec78f205c76af
HijoDelDiablo <brunoj_22@hotmail.com>
2015-10-08 15:02:34 -0300

    el servidor responde con los datos

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,20 +1,34 @@
-app.controller('twitterController',['$scope', 'twitterFactory',function ($scope, twitterFactory){
-	/*$scope.title = "Holi";*/
-	var tweets = function() {
-		/*$scope.loading = true;
-		twitterFactory.query({
-			url: 'http://localhost:3000/timeline',},
-			function(data){
-				$scope.loading = false;
-			});
-		});*/
-	};
+app.controller('twitterController',['$scope', 'twitterFactory', '$http',function ($scope, twitterFactory, $http){
 
-	var call = function(b) {
-		console.log("ajax done");
+		var getTweets = function(b) {
+			console.log("ajax is working");
+			var tweets = [],
+
+			/*$http.get('http://localhost:3000/timeline', function(data) {
+			params = {
+				count: 10
+			};*/
+
+			tweets = b;
+			var i = 0, Twlen = tweets.data.length;
+
+			if(Twlen) {
+				for (i; i < Twlen; i++) {
+					console.log("tweet" + i + " = " + tweets.data[i].user.screen_name);
+				};
+			};
+
+			console.log(tweets)
+			return tweets;
+		};
+		twitterFactory.goHome(getTweets);
+		
+
+	/*var call = function(b) {
+		console.log("ajax work");
 		console.log(b);
 		return b;
 	};
 
-	twitterFactory.goHome(call);
+	twitterFactory.goHome(call);*/
 }]);
